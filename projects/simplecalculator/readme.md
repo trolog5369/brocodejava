cb4aa85994c8bb2394cc18baa7550058ba2cc9dd
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-10-25 23:32:36 +0530

    Added mini project: Simple Calculator using enhanced switch expressions and input validation

---
+++ projects/simplecalculator/readme.md
@@ -0,0 +1,27 @@
+### 🧮 Simple Calculator
+**File:** `simplecalculator.java`  
+**Concepts Used:**  
+- Enhanced Switch Expression (Java 14 feature)  
+- Arithmetic Operations  
+- Input Validation (Division by Zero Handling)  
+- Character Extraction from String (`scanner.next().charAt(0)`)  
+- Boolean Flags for Flow Control  
+
+**Description:**  
+A simple command-line calculator that performs basic arithmetic operations (`+`, `-`, `*`, `/`, `^`) using **enhanced switch expressions**.  
+It also validates user input to prevent division by zero and invalid operators.  
+
+**Sample Input/Output:**
+Enter the first number: 8
+Enter an operator(+,-,*,/,^): /
+Enter the second number: 0
+Cannot divide by zero!
+
+Enter the first number: 5
+Enter an operator(+,-,*,/,^): *
+Enter the second number: 4
+20.0
+
+
+**Learning Outcome:**  
+Reinforced knowledge of enhanced switches, user input handling, operator-based branching, and error prevention logic.
---
+++ projects/simplecalculator/simplecalculator.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+public class simplecalculator {
+public static void main(String[] args) {
+    // Simple calculator using enhance switches
+
+    Scanner scanner=new Scanner(System.in);
+
+    double num1;
+    double num2;
+    char operator;
+    double result=0;
+    boolean validOperation=true;
+
+    System.out.print("Enter the first number: ");
+    num1=scanner.nextDouble();
+
+    System.out.println("Enter an operator(+,-,*,/,^): ");
+    operator=scanner.next().charAt(0);
+    // This above line will only now capture the first char insated of the whole string if entered by user
+
+    System.out.print("Enter the second number: ");
+    num2=scanner.nextDouble();
+
+    // Using enhance switch now
+    switch(operator){
+        case '+' ->result=num1+num2;
+        case '-' ->result=num1-num2;
+        case '*' ->result=num1*num2;
+        case '^' ->result=Math.pow(num1, num2);
+        // case '/' ->result=num1/num2;
+        // What if someone divides by 0 it gives "Infinity" as a result if we want to prevent that we can use if else
+        case '/' ->{
+            if(num2==0){
+                System.out.println("Cannot divide by zero!");
+                validOperation=false;
+                // beacuse we dont wanna continue and display the result as its showing
+                // Cannot divide by zero!
+                //0.0-_> This result 
+
+            }
+            else{
+                result=num1/num2;
+            }
+        }
+    default ->{
+        System.out.println("Invalid operator!");
+        validOperation=false;
+    }
+}
+if(validOperation==true){
+    System.out.println(result);
+}
+    scanner.close();
+}
+}
