f3cad7eaf130934bfa418283d15e4c1f341abe27
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-11-13 19:06:41 +0530

    Added Cat.java — Child class of Animal showcasing inheritance and unique behavior 'speak()' and attribute 'lives'.

---
+++ Week4/day05/Cat.java
@@ -0,0 +1,14 @@
+// This is a child class of the animal
+public class Cat extends Animal {
+// for a child class to iherit the attributes and methods of the parent we use the "extends" keyword
+    
+// Now this class has inherit the properties of the parent clas ie Animal class
+
+// Each child class could have their own unique attrivbutes and methods too
+
+// unique attributes and method for Cat class
+int lives=9;
+void speak(){
+    System.out.println("The cat goes meow!");
+}
+}
