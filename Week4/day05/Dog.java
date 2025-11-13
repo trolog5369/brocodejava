dc21220f01486fad2c30298f67626b2d1a3fe106
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-11-13 19:05:35 +0530

    Added Dog.java — Child class of Animal demonstrating inheritance and unique behavior 'speak()' and attribute 'lives'.

---
+++ Week4/day05/Dog.java
@@ -0,0 +1,15 @@
+// This is a child class of the animal
+public class Dog extends Animal{
+// for a child class to iherit the attributes and methods of the parent we use the "extends" keyword
+    
+// Now this class has inherit the properties of the parent clas ie Animal class
+
+// Each child class could have their own unique attrivbutes and methods too
+
+// unique attributes and method for Dog class
+int lives=1;
+void speak(){
+    System.out.println("The dog goes woff!");
+}
+
+}
