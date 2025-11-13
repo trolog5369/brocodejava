69e6f8214540786011a8a11147083b2d20d1b253
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-11-13 18:55:36 +0530

    Added Organism.java — Base grandparent class defining core attribute 'isAlive' and constructor for inheritance demonstration.

---
+++ Week4/day05/Organism.java
@@ -0,0 +1,8 @@
+
+// This is a grandparent class for parent class Animal hence we used extand keyword
+public class Organism {
+        boolean isAlive;
+    Organism(){
+        isAlive=true;
+    }
+}
