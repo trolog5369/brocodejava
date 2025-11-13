58a88369a891c1200d476de82752e5a567d11b2e
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-11-13 18:58:02 +0530

    Added Animal.java — Parent class extending Organism and defining shared behavior 'eat()' for child classes.

---
+++ Week4/day05/Animal.java
@@ -0,0 +1,13 @@
+
+// this is a child class of the grandparent class Organism
+// This is a parent class of Dog and Class(child classes)
+public class Animal extends Organism {
+
+    void eat(){
+        System.out.println("The animal is eating");
+    }
+}
+
+// WE craeted an animal class which will act as parent class 
+// of the dog and cat class
+// for a child class to iherit the attributes and methods of the parent we use the "extends" keyword
