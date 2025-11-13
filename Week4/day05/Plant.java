ab63432444049747d2961dc1d890bdc74ffdcd4c
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-11-13 19:06:59 +0530

    Added Plant.java — Independent child class of Organism with its own unique method 'photosynthesis()' to demonstrate sibling inheritance.

---
+++ Week4/day05/Plant.java
@@ -0,0 +1,7 @@
+
+// This is a child class of the organism class
+public class Plant extends Organism {
+    void photosyntheisis(){
+        System.out.println("The plant absorb sunlight");
+    }
+}
